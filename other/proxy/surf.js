b6d680530e80ece455475b3311d90f8247e6e5fe
lurk <84330709+Sxri@users.noreply.github.com>
2023-02-18 19:50:42 -0800

    Create surf.js

---
+++ other/proxy/surf.js
@@ -0,0 +1,2 @@
+function surf(value){let iframe=document.querySelector(".iframe.active");window.navigator.serviceWorker.register("./sw.js",{scope:__uv$config.prefix,}).then(()=>{let url=value.trim();if(!isUrl(url))url="https://www.google.com/search?q="+url;else if(!(url.startsWith("https://")||url.startsWith("http://")))
+url="https://"+url;sessionStorage.setItem("encodedUrl",__uv$config.encodeUrl(url));location.href="surf.html";});}
